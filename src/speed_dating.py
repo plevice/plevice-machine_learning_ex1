cc5cdd8d27d452b5c78bc3834f77bfbb502874e9
Christoph Jandl <e11808249@student.tuwien.ac.at>
2022-11-07 11:31:23 +0100

    Add cv to training.py

--- src/speed_dating.py
+++ src/speed_dating.py
@@ -1,10 +1,16 @@
 import numpy as np
 from sklearn.datasets import fetch_openml
-from sklearn.impute import KNNImputer
-from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import KNNImputer, SimpleImputer, IterativeImputer
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler, Normalizer
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
+from classify import classify
+from preprocess import preprocess
 from src.utils.training import do_training
 
 
@@ -16,11 +22,15 @@ def main():
 
     df = dataset.frame
     del df['has_null']
+    # 78.5% missing values -> keep or not?
+    del df["expected_num_interested_in_me"]
     convert_categories_to_boolean(df, ["samerace", "match"])
 
     # handle missing numeric values
     numeric_features_minmax = df.select_dtypes(
         include=np.number).columns.tolist()
+    # already scaled
+    numeric_features_minmax.remove("interests_correlate")
     numeric_features_zscale = ["d_age", "age"]
 
     for attr in numeric_features_zscale:
@@ -33,28 +43,37 @@ def main():
 
     steps = [
         ("num_z", [
-            ("imputer", KNNImputer(n_neighbors=8)),
+            ("imputer", KNNImputer(n_neighbors=1)),
             ("scaler", StandardScaler())
         ], numeric_features_zscale),
 
         ("num_min_max", [
-            ("imputer", KNNImputer(n_neighbors=8)),
+            ("imputer", KNNImputer(n_neighbors=1)),
             ("scaler", MinMaxScaler())
         ], numeric_features_minmax),
 
         ("cat", [
-            #        ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
             ("encode", OneHotEncoder(handle_unknown="ignore"))
         ], categorical_features),
     ]
 
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=0.2, random_state=10)
+    print(f"Preprocessing steps are:")
+    for step in steps:
+        print(f"{step[0]} -> {step[1]}")
+
     for c in [
-        RandomForestClassifier(),
-        # RandomForestClassifier(min_samples_split=3),
+        # BernoulliNB(),
+        # MultinomialNB(),
+        # RandomForestClassifier(),
+        DecisionTreeClassifier(max_depth=10),
+        RandomForestClassifier(min_samples_split=3),
+        KNeighborsClassifier(n_neighbors=4)
     ]:
-        do_training(x_train, y_train, x_test, y_test, steps, c)
+        do_training(x, y, steps, c, cv=5)
+        # do_training(x, y, steps, c, test_size=0.1)
+
+
+#  print(scores_dict)
 
 
 def convert_categories_to_boolean(df, attrs):
--- src/utils/training.py
+++ src/utils/training.py
@@ -1,19 +1,31 @@
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
+import pandas as pd
+
+from src.utils.pipeline_debugger import Debugger
 
 
-def do_training(x_train, y_train, x_test, y_test, preprocessing_steps, classifier):
+def do_training(x, y, preprocessing_steps, classifier, cv=None, test_size=0.2):
+    if cv is not None and cv < 2:
+        raise ValueError(f"cv {cv} is not valid!")
+
     trainer = DatasetTrainer()
     for step in preprocessing_steps:
         trainer.add_preprocessing_step(step[0], step[1], step[2])
 
-    print("Training dataset with {}...".format(classifier.__repr__()), end="")
-    trainer.train(classifier, x_train, y_train)
-    print("Done.")
-    print("Testing dataset...", end="")
-    score = trainer.test(x_test, y_test)
-    print("done. Model score: %.3f" % score)
+    print("Training dataset with ",
+          f"{classifier.__repr__()}, cv={cv}, test_size={test_size if cv is None else 1/cv}...", end="")
+    if cv:
+        scores = trainer.cross_validate(classifier, x, y, cv=cv)
+        print(f"done.\nModel scores: {scores}")
+    else:
+        X_train, X_test, y_train, y_test = train_test_split(x, y, random_state=0, test_size=test_size)
+        trainer.train(classifier, X_train, y_train)
+        print("Testing dataset...", end="")
+        score = trainer.test(X_test, y_test)
+        print("Done.")
+        print("done. Model score: %.5f" % score)
 
 
 class DatasetTrainer:
@@ -31,7 +43,15 @@ class DatasetTrainer:
 
         return self.clf.score(x_test, y_test)
 
+    def cross_validate(self, classifier, x, y, cv=10):
+        self.__build_classifier(classifier)
+        return cross_val_score(self.clf, x, y, cv=cv)
+
     def train(self, classifier, x_train, y_train):
+        self.__build_classifier(classifier)
+        return self.clf.fit(x_train, y_train)
+
+    def __build_classifier(self, classifier):
         if len(self.transformers) == 0:
             raise Exception("No preprocessing steps added!")
 
@@ -43,12 +63,8 @@ class DatasetTrainer:
             steps=[
                 ("preprocessor", preprocessor),
 
-          #      ("f", FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
-                #      ("debug", Debugger()),
+                #      ("f", FunctionTransformer(lambda x: x.todense(), accept_sparse=True)),
+                #  ("debug", Debugger()),
                 ("classifier", classifier)
             ]
         )
-
-       #  print(cross_val_score(self.clf, x_train, y_train, cv=10))
-
-        return self.clf.fit(x_train, y_train)
